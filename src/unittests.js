92ec726fc729ba58c25f603d8ed339e9c61fef0e
Jim Cramer <remarcmij@gmail.com>
2017-09-12 22:48:27 +0200

    fixed merge conflict

--- src/unittests.js
+++ src/unittests.js
@@ -1,8 +1,4 @@
 export { default as initialState } from './store/init'
-<<<<<<< HEAD
 export { execute, execProg } from './processor/controlUnit'
-=======
-export { execute } from './processor/controlUnit'
->>>>>>> 529ac1eb5e625f5e56fc3db2de0ba76c8cfdf539
 import * as C from './processor/keyCodes'
 export { C }
