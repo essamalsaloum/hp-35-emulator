7f910bbe7913501c9e4283e40d0afdb2bb842142
Jim Cramer <remarcmij@gmail.com>
2017-09-27 18:57:10 +0200

    styling fixes

--- src/containers/Display.js
+++ src/containers/Display.js
@@ -7,19 +7,6 @@ import './Display.css'
 
 const labels = ['x', 'y', 'z', 't']
 
-const styles = {
-  root: {
-    margin: '0 0 2px 0',
-    padding: 8,
-    borderRadius: 4,
-    backgroundColor: '#f0f0f0',
-    boxShadow: 'inset 0 0 4px #888'
-  },
-  displayRow: {
-    margin: '0 8px'
-  }
-}
-
 class Display extends React.PureComponent {
 
   static propTypes = {
@@ -30,10 +17,6 @@ class Display extends React.PureComponent {
   renderStack(stack, buffer) {
     return stack.map((register, index) => {
       const value = index === 0 ? buffer : math.format(register, { precision: 14 })
-      // const style = { ...styles.displayRow }
-      // if (index > 0) {
-      //   style.color = '#808080'
-      // }
       return (
         <div key={index} className="Display--row">
           {`${labels[index]}: ${value}`}
