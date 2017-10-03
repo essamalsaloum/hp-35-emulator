3a53960fedb2ac25c246a530fe82b3d9b9bb0437
Jim Cramer <remarcmij@gmail.com>
2017-10-03 19:28:20 +0200

    preserve y for % and %chg

--- src/cpu/instructions/math.js
+++ src/cpu/instructions/math.js
@@ -1,5 +1,6 @@
 import C from '../keyCodes'
 import math from 'mathjs'
+import flow from 'lodash/flow'
 
 const degreesToRadians = degrees => degrees * math.PI / 180.0
 const radiansToDegrees = radians => radians * 180.0 / math.PI
@@ -12,36 +13,40 @@ const degrees360 = angle => {
   return angle
 }
 
-const monadic = fn => ([x, y, z, t]) => [fn(x), y, z, t]
-const dyadic = fn => ([x, y, z, t]) => [fn(x, y), z, t, t]
-const dyadicPreserveY = fn => ([x, y, z, t]) => [fn(x, y), y, z, t]
-
 const arithmetic = {
-  [C.ADD]: dyadic((x, y) => y + x),
-  [C.SUB]: dyadic((x, y) => y - x),
-  [C.MUL]: dyadic((x, y) => y * x),
-  [C.DIV]: dyadic((x, y) => x === 0 ? new Error('division by 0') : y / x),
-  [C.INV]: monadic(x => x === 0 ? new Error('division by 0') : 1 / x),
-  [C.IP]: monadic(x => math.floor(x)),
-  [C.PCT]: dyadicPreserveY((x, y) => y * x / 100),
-  [C.PCTCHG]: dyadicPreserveY((x, y) => (x - y) * (100 / y))
+  [C.ADD]: ([x, y, z, t]) => [y + x, z, t, t],
+  [C.SUB]: ([x, y, z, t]) => [y - x, z, t, t],
+  [C.MUL]: ([x, y, z, t]) => [y * x, z, t, t],
+  [C.DIV]: ([x, y, z, t]) => x === 0 ? new Error('division by 0') : [y / x, z, t, t],
+  [C.INV]: ([x, ...rest]) => x === 0 ? new Error('division by 0') : [1 / x, ...rest],
+  [C.IP]: ([x, ...rest]) => [math.floor(x), ...rest],
+  [C.PCT]: ([x, y, ...rest]) => [y * x / 100, y, ...rest],
+  [C.PCTCHG]: ([x, y, ...rest]) => [(x - y) * (100 / y), y, ...rest]
 }
 
+const abs = x => math.abs(x) > 1 ? new Error('invalid data') : radiansToDegrees(math.acos(x))
+const asin = x => math.abs(x) > 1 ? new Error('invalid data') : radiansToDegrees(math.asin(x))
+const atan = x => radiansToDegrees(math.atan(x))
+const cos = x => math.abs(degrees360(x) - 90) % 180 === 0 ? 0 : math.cos(degreesToRadians(degrees360(x)))
+const sin = x => math.sin(degreesToRadians(degrees360(x)))
+const sqrt = x => x < 0 ? new Error('√(negative)') : math.sqrt(x)
+const tan = x => math.abs(degrees360(x) - 90) % 180 === 0 ? NaN : math.tan(degreesToRadians(degrees360(x)))
+
 const transcendental = {
-  [C.ACOS]: monadic(x => math.abs(x) > 1 ? new Error('invalid data') : radiansToDegrees(math.acos(x))),
-  [C.ALOG]: monadic(x => math.pow(10, x)),
-  [C.ASIN]: monadic(x => math.abs(x) > 1 ? new Error('invalid data') : radiansToDegrees(math.asin(x))),
-  [C.ATAN]: monadic(x => radiansToDegrees(math.atan(x))),
-  [C.COS]: monadic(x => math.cos(degreesToRadians(degrees360(x)))),
-  [C.EXP]: monadic(x => math.exp(x)),
-  [C.LN]: monadic(x => math.log(x)),
-  [C.LOG]: monadic(x => math.log10(x)),
-  [C.POW]: dyadic((x, y) => math.pow(y, x)),
-  [C.ROOT]: dyadic((x, y) => math.pow(y, 1 / x)),
-  [C.SIN]: monadic(x => math.sin(degreesToRadians(degrees360(x)))),
-  [C.SQ]: monadic(x => x * x),
-  [C.SQRT]: monadic(x => x < 0 ? new Error('√(negative)') : math.sqrt(x)),
-  [C.TAN]: monadic(x => math.abs(degrees360(x) - 90) % 180 === 0 ? NaN : math.tan(degreesToRadians(degrees360(x))))
+  [C.ACOS]: ([x, ...rest]) => [abs(x), ...rest],
+  [C.ALOG]: ([x, ...rest]) => [math.pow(10, x), ...rest],
+  [C.ASIN]: ([x, ...rest]) => [asin(x), ...rest],
+  [C.ATAN]: ([x, ...rest]) => [atan(x), ...rest],
+  [C.COS]: ([x, ...rest]) => [cos(x), ...rest],
+  [C.EXP]: ([x, ...rest]) => [math.exp(x), ...rest],
+  [C.LN]: ([x, ...rest]) => [math.log(x), ...rest],
+  [C.LOG]: ([x, ...rest]) => [math.log10(x), ...rest],
+  [C.POW]: ([x, y, z, t]) => [math.pow(y, x), z, t, t],
+  [C.ROOT]: ([x, y, z, t]) => [math.pow(y, 1 / x), z, t, t],
+  [C.SIN]: ([x, ...rest]) => [sin(x), ...rest],
+  [C.SQ]: ([x, ...rest]) => [x * x , ...rest],
+  [C.SQRT]: ([x, ...rest]) => [sqrt(x), ...rest],
+  [C.TAN]: ([x, ...rest]) => [tan(x), ...rest]
 }
 
 const statistical = {
@@ -55,11 +60,11 @@ const funcs = {
 }
 
 const compute = keyCode => state => {
-  let { stack } = state
   const fn = funcs[keyCode]
   if (!fn) {
     throw new Error(`math: not implemented [${keyCode}]`)
   }
+  const { stack } = state
   return { ...state, stack: fn(stack) }
 }
 
