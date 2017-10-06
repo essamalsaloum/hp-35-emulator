b1c4ef9d3ac6bdf9db1103571236444cf370561a
Jim Cramer <remarcmij@gmail.com>
2017-10-06 14:26:41 +0200

    fixed error handling

--- src/services/github.js
+++ src/services/github.js
@@ -2,26 +2,32 @@ import axios from 'axios'
 
 const REPO_URL = `https://api.github.com/repos/RPNext35/rpnext35-programs/contents/programs`
 
-const headers = {
-  Accept: 'application/vnd.github.v3+json'
+const config = {
+  headers: {
+    Accept: 'application/vnd.github.v3+json'
+  },
+  timeout: 2000
 }
 
 const cache = new Map()
 
+const createFetchError = error => {
+  if (error.response) {
+    return new Error(`GitHub response: ${error.response.data.message}`)
+  }
+  if (error.request) {
+    return new Error('GitHub did not respond')
+  }
+  return new Error(error.message)
+}
+
 export const fetchFileList = path => {
   const url = path ? `${REPO_URL}/${path}` : REPO_URL
-  console.log(url)
   const cachedPrograms = cache.get(url)
   if (cachedPrograms) {
-    return new Promise((resolve, reject) => {
-      try {
-        resolve(cachedPrograms)
-      } catch (err) {
-        reject(err)
-      }
-    })
+    return Promise.resolve(cachedPrograms)
   }
-  return axios.get(url, { headers })
+  return axios.get(url, config)
     .then(res => {
       const programs = res.data
         .filter(item => item.name.endsWith('.md'))
@@ -38,27 +44,30 @@ export const fetchFileList = path => {
       cache.set(url, programs)
       return programs
     })
+    .catch(error => {
+      throw createFetchError(error)
+    })
 }
 
 export const fetchProgramText = (url, sha) => {
-  const cachedItem = cache.get(url)
+  const cachedItem = window.localStorage.getItem(url)
   if (cachedItem) {
-    return new Promise((resolve, reject) => {
-      try {
-        const data = JSON.parse(cachedItem)
-        if (data.sha === sha) {
-          const { text } = data
-          resolve(text)
-        }
-      } catch (err) {
-        reject(err)
+    try {
+      const data = JSON.parse(cachedItem)
+      if (data.sha === sha) {
+        return Promise.resolve(data.text)
       }
-    })
+    } catch (err) {
+      // fall thru
+    }
   }
-  return axios.get(url, { headers })
+  return axios.get(url, config)
     .then(res => {
       const text = res.data
-      cache.set(url, JSON.stringify({ text, sha }))
+      window.localStorage.setItem(url, JSON.stringify({ text, sha }))
       return text
+    })
+    .catch(error => {
+      throw createFetchError(error)
     })
 }
