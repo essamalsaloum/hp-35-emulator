440affe5ee86c91054080bd259b769b20c83e4d1
Jim Cramer <remarcmij@gmail.com>
2017-10-16 13:44:49 +0200

    reinstated throttled persistence of state

--- src/Root.js
+++ src/Root.js
@@ -3,9 +3,19 @@ import { Provider } from 'react-redux'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import { indigo500, indigo700 } from 'material-ui/styles/colors'
+import throttle from 'lodash/throttle'
 import { saveState } from './localStorage'
 import store from './store'
 import App from './App'
+
+const THROTTLE_WAIT_MS = 1000
+
+const saveStateHelper = () => {
+  const { cpu, program, preferences } = store.getState()
+  saveState({ cpu, program, preferences })
+}
+
+const saveStateThrottled = throttle(saveStateHelper, THROTTLE_WAIT_MS)
 
 const muiTheme = getMuiTheme({
   palette: {
@@ -26,9 +36,10 @@ class Root extends React.PureComponent {
   }
 }
 
-window.onbeforeunload = () => {
-  const { cpu, program, preferences } = store.getState()
-  saveState({ cpu, program, preferences })
-}
+store.subscribe(() => {
+  saveStateThrottled()
+})
+
+window.onbeforeunload = saveStateHelper
 
 export default Root
